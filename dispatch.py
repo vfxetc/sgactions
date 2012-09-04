f0f688bc7763690d2855c84ab33041c69a04f913
Mike Boers <mboers@keypics.com>
2012-09-04 12:05:55 -0700

    Parse IDs and convert some lists to scalars

--- dispatch.py
+++ dispatch.py
@@ -11,18 +11,28 @@ def main(url):
     scheme, path, query = m.groups()
     query = urlparse.parse_qs(query, keep_blank_values=True) if query else {}
     
+    # Parse the values.
+    for k, v in query.items():
+        if k == 'ids' or k.endswith('_ids'):
+            v[:] = [int(x) for x in v[0].split(',')]
+            continue
+        if k.endswith('_id'):
+            v[:] = [int(x) for x in v]
+        if len(v) == 1 and k not in ('cols', 'column_display_names'):
+            query[k] = v[0]
+    
     # Parse the path into an entrypoint.
-    m = re.match(r'^([\w.]+)(?::(\w+))?$', path)
+    m = re.match(r'^([\w.]+):(\w+)$', path)
     if not m:
         print >>sys.stderr, 'entrypoint must be like "package.module:function"'
         exit(1)
     module_name, function_name = m.groups()
     
     # Load the module, and dispatch to the function.
+    # TODO: Catch errors, and log them to Shotgun.
     module = __import__(module_name, fromlist=['.'])
-    if function_name:
-        function = getattr(module, function_name)
-        function(**query)
+    function = getattr(module, function_name)
+    function(**query)
 
 
 def test_kwargs(**kwargs):
