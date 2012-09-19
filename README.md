547a790426ba5062cb782db677c5bbb618bab4aa
Mike Boers <mboers@keypics.com>
2012-09-19 11:35:25 -0700

    Add lots of project metadata

---
+++ README.md
@@ -0,0 +1,7 @@
+# sgactions
+
+This Python package is a wrapper around `ActionMenuItems` in [Shotgun](http://www.shotgunsoftware.com) providing a simple interface to Python callables via a [YAML](http://www.yaml.org/) configuration.
+
+It also has an optional [Greasemonkey](http://en.wikipedia.org/wiki/Greasemonkey)-style user script to allow to injection of icons, and headings.
+
+[Read the docs](http://sgactions.readthedocs.org/), and good luck!
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='sgactions',
+    version='0.1-dev',
+    description='Shotgun ActionMenuItem enrichment.',
+    url='http://github.com/westernx/sgactions',
+    
+    packages=['sgactions'],
+    
+    author='Mike Boers',
+    author_email='sgactions@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
