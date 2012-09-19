515bbcc1b2029ef75218b9052f200469662bdaf8
Mike Boers <mboers@keypics.com>
2012-09-19 11:33:20 -0700

    Organize into a package

--- sgactions/register.py
+++ sgactions/register.py
@@ -1,26 +1,28 @@
 """Register the protocol handler on various OSes."""
 
 from subprocess import call, PIPE
-import platform
 import os
+import platform
 
 
 def main():
     
     if platform.system() == 'Darwin':
+        print 'Running "lsregister" ...'
         call([
             '/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister',
             '-f',
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'applescript_handler.app'),
+            os.path.abspath(os.path.join(__file__, '..', '..', 'applescript_handler.app')),
         ])
+        print 'Done.'
     
     elif platform.system() == 'Linux':
         call([
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'register-linux.sh'),
+            os.path.abspath(os.path.join(__file__, '..', '..', 'register-linux.sh')),
         ])
     
     else:
-        print 'We are not setup for procotol handlers on %s' % platform.system()
+        print 'We are not setup for protocol handlers on %s' % platform.system()
 
 
 if __name__ == '__main__':
