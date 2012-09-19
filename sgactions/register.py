eacda7ab6f86b63180a88a4a29adf106ae3f7408
Mike Boers <mboers@keypics.com>
2012-09-19 13:24:02 -0700

    Be less agressive about re-registering

--- sgactions/register.py
+++ sgactions/register.py
@@ -9,18 +9,19 @@ def main():
     
     if platform.system() == 'Darwin':
         lsregister = '/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister'
+        handler = os.path.abspath(os.path.join(__file__, '..', '..', 'Shotgun Action Dispatcher.app'))
         print 'Cleaning old handlers...'
         proc = Popen([lsregister, '-dump'], stdout=PIPE, stderr=PIPE)
         for line in proc.stdout:
             m = re.match(r'\s*path:\s*(.+?/sgactions/.+?\.app)\s*$', line)
-            if m:
+            if m and m.group(1) != handler:
                 print m.group(1)
                 call([lsregister, '-u', m.group(1)], stdout=PIPE, stderr=PIPE)
         print 'Registering new handler...'
         call([
             lsregister,
-            '-v', '-f',
-            os.path.abspath(os.path.join(__file__, '..', '..', 'Shotgun Action Dispatcher.app')),
+            '-v',
+            handler,
         ])
         print 'Done.'
     
