fcb6fa8416f3723150b8680ed1cd0aa3b74fb269
Mike Boers <mboers@keypics.com>
2012-09-25 12:32:46 -0700

    Catch all errors in dispatch

--- sgactions/dispatch.py
+++ sgactions/dispatch.py
@@ -10,30 +10,30 @@ from . import tickets
 
 def main(url):
     
-    # Parse the URL into scheme, path, and query.
-    m = re.match(r'^(?:(\w+):)?(.*?)(?:/(.*?))?(?:\?(.*))?$', url)
-    scheme, netloc, path, query = m.groups()
-    query = urlparse.parse_qs(query, keep_blank_values=True) if query else {}
-    
-    # Parse the values.
-    for k, v in query.items():
-        if k == 'ids' or k.endswith('_ids'):
-            v[:] = [int(x) for x in v[0].split(',')] if v[0] else []
-            continue
-        if k.endswith('_id'):
-            v[:] = [int(x) for x in v]
-        if len(v) == 1 and k not in ('cols', 'column_display_names'):
-            query[k] = v[0]
-    
-    # Parse the path into an entrypoint.
-    m = re.match(r'^([\w.]+):(\w+)$', netloc)
-    if not m:
-        print >>sys.stderr, 'entrypoint must be like "package.module:function"'
-        exit(1)
-    module_name, function_name = m.groups()
-    
     try:
-        
+    
+        # Parse the URL into scheme, path, and query.
+        m = re.match(r'^(?:(\w+):)?(.*?)(?:/(.*?))?(?:\?(.*))?$', url)
+        scheme, netloc, path, query = m.groups()
+        query = urlparse.parse_qs(query, keep_blank_values=True) if query else {}
+    
+        # Parse the values.
+        for k, v in query.items():
+            if k == 'ids' or k.endswith('_ids'):
+                v[:] = [int(x) for x in v[0].split(',')] if v[0] else []
+                continue
+            if k.endswith('_id'):
+                v[:] = [int(x) for x in v]
+            if len(v) == 1 and k not in ('cols', 'column_display_names'):
+                query[k] = v[0]
+    
+        # Parse the path into an entrypoint.
+        m = re.match(r'^([\w.]+):(\w+)$', netloc)
+        if not m:
+            print >>sys.stderr, 'entrypoint must be like "package.module:function"'
+            exit(1)
+        module_name, function_name = m.groups()
+    
         # Load the module, and dispatch to the function.
         module = __import__(module_name, fromlist=['.'])
         function = getattr(module, function_name)
